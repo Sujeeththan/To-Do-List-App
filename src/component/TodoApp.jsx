8230ff14b5a36b2a6fb1fd16374d240643b30754
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-21 22:22:25 +0530

    function add

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,20 +1,40 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 function TodoApp() {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState([]);
 
+  const initialTodos = [];
+  for (let i = 0; i < 100; i++) {
+    initialTodos.push({
+      id: i,
+      text: 'Item ' 
+    });
+  }
+
 function addTask() {
   setTask(task);
-  alert("Enter the Task")
+  if(!task){
+   alert("Enter the Task")
+  }
 }
 
+function deleteTask() {
+  setTodos(todos);
+}
   return (
     <div className='container'>
       <h2 className='head'>My To-Do List</h2>
       <input type="text" placeholder='Enter a new task...'  />
       <button onClick={addTask}>Add Task</button>
-      <div></div>
+      <div > 
+        <ul className='todo-list'>
+           <li class="todo-item">
+              <span ></span>
+          </li> 
+        </ul>
+        </div>
     </div>
   )
 }
