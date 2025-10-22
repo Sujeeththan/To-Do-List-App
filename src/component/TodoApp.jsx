af537cba124b566988bb6ab9eefffa2da6554c16
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-22 06:38:41 +0530

    todo function and filter

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function TodoApp() {
   const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todosList, setTodoslist] = useState([]);
 
-  const initialTodos = [];
-  for (let i = 0; i < 100; i++) {
-    initialTodos.push({
-      id: i,
-      text: 'Item ' 
-    });
+  const addTodo = () => {
+    if (task.trim() === "") {
+      alert("Enter a task...");
+      return;
+    }
+    setTodoslist([...todosList, { id: Date.now(), text: task }]);
+    setTask(""); 
+  };
+
+  const  deleteTodo = (id) => {
+    setTodoslist(
+      todosList.filter((item) => item.id !== id )
+    )
   }
 
-function addTask() {
-  setTask(task);
-  if(!task){
-   alert("Enter the Task")
-  }
-}
-
-function deleteTask() {
-  setTodos(todos);
-}
   return (
-    <div className='container'>
-      <h2 className='head'>My To-Do List</h2>
-      <input type="text" placeholder='Enter a new task...'  />
-      <button onClick={addTask}>Add Task</button>
-      <div > 
-        <ul className='todo-list'>
-           <li class="todo-item">
-              <span ></span>
-          </li> 
+    <div className="container">
+      <h2 className="head">My To-Do List</h2>
+      <input
+        type="text"
+        placeholder="Enter a new task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button id='btn' onClick={addTodo}>Add Task</button>
+      <div>
+        <ul className="todo-list">
+          {todosList.map((item) => (
+            <li className="todo-item" key={item.id}>
+              {item.text}
+              <span>{item.text}</span>
+              <button onClick={(e) => {
+                e.deleteTodo(item.id);
+              }}>Del</button>
+            </li>
+          ))}
         </ul>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TodoApp
+export default TodoApp;
