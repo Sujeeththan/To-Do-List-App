93bc5f18364f624890721584523baa7ca64cd3e9
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-22 14:29:19 +0530

    Duplicate add

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function TodoApp() {
   const [task, setTask] = useState("");
   const [todosList, setTodoslist] = useState([]);
 
-  const addTodo = () => {
+  const addTodo = (task) => {
     if (task.trim() === "") {
       alert("Enter a task...");
+    }
+    const isDuplicate = todosList.some(
+      (value) => value.trim().toLowerCase() == task.trim().toLowerCase()
+    ); // true or false
+    if (isDuplicate) {
+      alert("Task already exist");
       return;
     }
+
     setTodoslist([...todosList, { id: Date.now(), text: task }]);
-    setTask(""); 
+    setTask("");
   };
 
-  const  deleteTodo = (id) => {
-    setTodoslist(
-      todosList.filter((item) => item.id !== id )
-    )
-  }
+  const deleteTodo = (id) => {
+    setTodoslist(todosList.filter((item) => item.id !== id));
+  };
 
   return (
     <div className="container">
@@ -28,20 +33,26 @@ function TodoApp() {
         value={task}
         onChange={(e) => setTask(e.target.value)}
       />
-      <button id='btn' onClick={addTodo}>Add Task</button>
+      <button id="btn" onClick={() => addTodo(task)}>
+        Add Task
+      </button>
       <div>
         {todosList.length > 0 && (
-        <ul className="todo-list">
-          {todosList.map((item) => (
-            <li className="todo-item" key={item.id}>
-              <span>{item.text}</span>
-              <button onClick={() => {
-                deleteTodo(item.id);
-              }}>Del</button>
-            </li>
-          ))}
-        </ul>
-      )}
+          <ul className="todo-list">
+            {todosList.map((item) => (
+              <li className="todo-item" key={item.id}>
+                <span>{item.text}</span>
+                <button
+                  onClick={() => {
+                    deleteTodo(item.id);
+                  }}
+                >
+                  Del
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
